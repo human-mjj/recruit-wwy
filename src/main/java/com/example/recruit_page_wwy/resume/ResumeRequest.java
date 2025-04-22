dc5796d342c1fc882475c6c7b571eb9008164510
son <son7571@github.com>
2025-04-22 17:51:34 +0900

    resume 상세ë³보기2

--- src/main/java/com/example/recruit_page_wwy/resume/ResumeRequest.java
+++ src/main/java/com/example/recruit_page_wwy/resume/ResumeRequest.java
@@ -8,8 +8,16 @@ public class ResumeRequest {
     public static class SaveDTO {
         private Integer user_id;
         private String title;
+        private String personalHistory;
+        private String experiencedDate;
         private String exp;
+
+
+        private String educationLevel;
+        private String schoolName;
         private String edu;
+
+
         private Integer job_id;
         private String region;
         private String regionDetail;
@@ -19,16 +27,38 @@ public class ResumeRequest {
         private String img_url;
 
 
-        public SaveDTO(Integer user_id, String title, String exp, String edu, Integer job_id, String qualified, String activity, String img_url) {
+        public SaveDTO(Integer user_id, String title, String exp, Integer job_id, String qualified, String activity, String img_url) {
             this.user_id = user_id;
             this.title = title;
             this.exp = exp;
-            this.edu = edu;
+            this.edu = edu(educationLevel, schoolName);
             this.job_id = job_id;
             this.location = location(region, regionDetail);
             this.qualified = qualified;
             this.activity = activity;
             this.img_url = img_url;
+        }
+
+        public String exp(String personalHistory) {
+            if (personalHistory == "inexperienced") {
+                return exp = personalHistory;
+            } else {
+                return exp = personalHistory + experiencedDate
+                        ;
+            }
+
+        }
+
+
+        public String edu(String educationLevel, String schoolName) {
+            if (schoolName != null && !schoolName.isBlank()) {
+                return educationLevel != null && !educationLevel.isBlank()
+                        ? educationLevel + " " + schoolName
+                        : schoolName;
+            } else {
+                return educationLevel != null ? educationLevel : "";
+            }
+
         }
 
         public String location(String region, String regionDetail) {
@@ -41,6 +71,8 @@ public class ResumeRequest {
             }
 
         }
+
+
     }
 
 
