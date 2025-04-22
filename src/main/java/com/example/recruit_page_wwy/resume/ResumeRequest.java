c0aca64159d313797ceebf20d467027a8af2f746
son <son7571@github.com>
2025-04-22 13:35:26 +0900

    resume1

--- src/main/java/com/example/recruit_page_wwy/resume/ResumeRequest.java
+++ src/main/java/com/example/recruit_page_wwy/resume/ResumeRequest.java
@@ -1,4 +1,32 @@
 package com.example.recruit_page_wwy.resume;
 
+import lombok.Data;
+
 public class ResumeRequest {
+
+    @Data
+    public static class SaveDTO {
+        private Integer user_id;
+        private String title;
+        private String exp;
+        private String edu;
+        private Integer job_id;
+        private String location;
+        private String qualified;
+        private String activity;
+        private String img_url;
+
+        public SaveDTO(Integer user_id, String title, String exp, String edu, Integer job_id, String location, String qualified, String activity, String img_url) {
+            this.user_id = user_id;
+            this.title = title;
+            this.exp = exp;
+            this.edu = edu;
+            this.job_id = job_id;
+            this.location = location;
+            this.qualified = qualified;
+            this.activity = activity;
+            this.img_url = img_url;
+        }
+    }
+
 }
--- src/main/java/com/example/recruit_page_wwy/resume/ResumeService.java
+++ src/main/java/com/example/recruit_page_wwy/resume/ResumeService.java
@@ -8,4 +8,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class ResumeService {
     private final ResumeRepository resumeRepository;
+
+    public void save(ResumeRequest.SaveDTO saveDTO) {
+        resumeRepository.save(saveDTO.getUser_id(), saveDTO.getTitle(), saveDTO.getExp(), saveDTO.getEdu(), saveDTO.getJob_id(), saveDTO.getLocation(), saveDTO.getQualified(),
+                saveDTO.getActivity(), saveDTO.getImg_url());
+
+    }
 }
