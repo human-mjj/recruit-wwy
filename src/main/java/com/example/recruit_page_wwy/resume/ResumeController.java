37229be68dfa984cc0f7cc79ed65457478e9b0c9
son7571 <sssk1755@gmail.com>
2025-04-23 14:27:16 +0900

    [feat] 이력서 쓰기 (#2)
    * 이력서 작성
    
    * 이력서 save 확인 (h2)
    
    ---------
    
    Co-authored-by: son <son7571@github.com>

--- src/main/java/com/example/recruit_page_wwy/resume/ResumeController.java
+++ src/main/java/com/example/recruit_page_wwy/resume/ResumeController.java
@@ -1,11 +1,49 @@
 package com.example.recruit_page_wwy.resume;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class ResumeController {
     private final ResumeService resumeService;
+    private final HttpSession session;
+
+    @GetMapping("/mypage/resume")
+    public String resumeList() {
+        return "resume/list";
+    }
+
+
+    @GetMapping("/resume/1")
+    public String resumeDetail() {
+        return "resume/detail";
+    }
+
+    @GetMapping("/resume/save-form")
+    public String resumeSaveForm() {
+        return "resume/save-form";
+    }
+
+    @PostMapping("/resume/save")
+    public String resumeSave(ResumeRequest.SaveDTO saveDTO) {
+        // 임시 테스트 user_id 설정
+        saveDTO.setUser_id(1);
+        System.out.println("입력값: " + saveDTO);
+        resumeService.save(saveDTO);
+
+        return "resume/list";
+    }
+
+    @GetMapping("/resume/1/update-form")
+    public String resumeUpdateForm() {
+        return "resume/update-form";
+    }
+
 
 }
+
+
--- src/main/java/com/example/recruit_page_wwy/resume/ResumeRepository.java
+++ src/main/java/com/example/recruit_page_wwy/resume/ResumeRepository.java
@@ -2,6 +2,7 @@ package com.example.recruit_page_wwy.resume;
 
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,17 @@ import org.springframework.stereotype.Repository;
 public class ResumeRepository {
     private final EntityManager em;
 
+    public void save(Integer user_id, String title, String exp, String edu, Integer job_id, String location, String qualified, String activity, String img_url) {
+        Query query = em.createNativeQuery("insert into resume_tb(user_id,title, exp, edu, job_id, location, qualified, activity, img_url) values (?,?,?,?,?,?,?,?,?)");
+        query.setParameter(1, user_id);
+        query.setParameter(2, title);
+        query.setParameter(3, exp);
+        query.setParameter(4, edu);
+        query.setParameter(5, job_id);
+        query.setParameter(6, location);
+        query.setParameter(7, qualified);
+        query.setParameter(8, activity);
+        query.setParameter(9, img_url);
+        query.executeUpdate();
+    }
 }
