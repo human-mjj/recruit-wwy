db2cf7c5b6e721f6a3c7f3fb6c85de830521d34d
Seo Hoejeong <139827011+clubnerdy@users.noreply.github.com>
2025-04-24 09:07:19 +0900

    [feat] 채용공고 관리 구현 (#6)
    * 세션 정보를 불러오지 못한 dummy data를 통한 테스트 진행
    
    * 채용공고 관리 구현
    
    ---------
    
    Co-authored-by: SeoHoejoeng <seohoejeong@gmail.com>

--- src/main/java/com/example/recruit_page_wwy/employment/EmploymentRepository.java
+++ src/main/java/com/example/recruit_page_wwy/employment/EmploymentRepository.java
@@ -2,12 +2,27 @@ package com.example.recruit_page_wwy.employment;
 
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
 public class EmploymentRepository {
     private final EntityManager em;
 
+    public List<Employment> findAllByUserId(Integer userId) {
+        String jpql = """
+            SELECT e FROM Employment e
+            JOIN FETCH e.user
+            JOIN FETCH e.job
+            WHERE e.user.id = :userId
+            ORDER BY e.id DESC
+        """;
+        return em.createQuery(jpql, Employment.class)
+                .setParameter("userId", userId)
+                .getResultList();
+    }
 }
