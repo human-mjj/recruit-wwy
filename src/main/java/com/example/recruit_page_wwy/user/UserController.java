57b51073db558e2724ba7b2b90d66463e7d0e76f
sxias <a32176740@gmail.com>
2025-04-24 11:26:31 +0900

    유저 정보 최종 확인 및 navbar 수정

--- src/main/java/com/example/recruit_page_wwy/user/UserController.java
+++ src/main/java/com/example/recruit_page_wwy/user/UserController.java
@@ -92,16 +92,5 @@ public class UserController {
         return "redirect:/";
     }
 
-        System.out.println(sessionUser.getUsername());
-        return "redirect:/";
-    }
-
-    // Logout
-    @GetMapping("/logout")
-    public String logout() {
-        session.invalidate();
-        return "redirect:/";
-    }
-
 
 }
