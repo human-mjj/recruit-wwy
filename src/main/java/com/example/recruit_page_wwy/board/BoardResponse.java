6ec6c9aeef68c167c43569d806f3cf80cde76a35
Seo Hoejeong <139827011+clubnerdy@users.noreply.github.com>
2025-04-29 17:08:10 +0900

    [feat] 커뮤니티 페이징(#31)
    * board paging complete
    
    ---------
    
    Co-authored-by: SeoHoejoeng <seohoejeong@gmail.com>
    Co-authored-by: parang <parang@naver.com>
    Co-authored-by: Sxias <a32176740@gmail.com>
    Co-authored-by: Sxias <106750460+Sxias@users.noreply.github.com>

--- src/main/java/com/example/recruit_page_wwy/board/BoardResponse.java
+++ src/main/java/com/example/recruit_page_wwy/board/BoardResponse.java
@@ -4,9 +4,55 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BoardResponse {
+
+    @Data
+    public static class ListDTO {
+        private List<Board> boards;
+        private Integer prev;
+        private Integer next;
+
+        private Integer size;
+        private Integer totalCount;
+        private Integer totalPage;
+        private Integer current;
+        private Boolean isFirst;
+        private Boolean isLast;
+        private List<Integer> numbers;
+
+        public ListDTO(List<Board> boards, Integer current, Integer totalCount) {
+            this.boards = boards;
+            this.size = 5;
+            this.prev = current - 1;
+            this.next = current + 1;
+            this.totalCount = totalCount;
+            this.totalPage = makeTotalPage(totalCount, size);
+            this.isFirst = current == 0;
+            this.isLast = (totalPage - 1) == current;
+            this.numbers = makeNumbers(current, totalPage);
+        }
+
+        private Integer makeTotalPage(int totalCount, int size) {
+            int rest = totalCount % size > 0 ? 1 : 0; //나머지값은 0~2를 순회함
+            return totalCount / size + rest;
+        }
+
+        private List<Integer> makeNumbers(int current, int totalPage) {
+            List<Integer> numbers = new ArrayList<>();
+
+            int start = (current / 5) * 5 + 1;
+            int end = Math.min(start + 4, totalPage);
+
+            for (int i = start; i <= end; i++) {
+                numbers.add(i);
+            }
+
+            return numbers;
+        }
+    }
 
     @Data
     public static class DetailDTO {
