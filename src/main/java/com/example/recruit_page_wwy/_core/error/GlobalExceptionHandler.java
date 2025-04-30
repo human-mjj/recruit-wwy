8f76a4a9e783202758740c05a98e87d913fcfcf4
PJumMin <129253604+PJumMin@users.noreply.github.com>
2025-04-30 15:44:06 +0900

    [feat] 로그인 인터셉터 완성 (#39)
    * intercetor 파일 생성
    
    * ex 폴더 생성
    
    * Login interceptor 완료
    
    ---------
    
    Co-authored-by: sxias <a32176740@gmail.com>

---
+++ src/main/java/com/example/recruit_page_wwy/_core/error/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.example.recruit_page_wwy._core.error;
+
+import com.example.recruit_page_wwy._core.error.ex.Exception400;
+import com.example.recruit_page_wwy._core.error.ex.Exception401;
+import com.example.recruit_page_wwy._core.error.ex.Exception403;
+import com.example.recruit_page_wwy._core.error.ex.Exception404;
+import com.example.recruit_page_wwy._core.util.Script;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    //Bad Request - 잘못된 요청 (400)
+    @ExceptionHandler(Exception400.class)
+    public String ex400(Exception e) {
+        return Script.back(e.getMessage());
+    }
+
+//    @ExceptionHandler(ExceptionApi400.class)
+//    public String exApi400(Exception e) {
+//        return Script.back(e.getMessage());
+//    }
+
+    //Unauthorized - 인증 안됨 (401)
+    @ExceptionHandler(Exception401.class)
+    public String ex401(Exception e) {
+        return Script.href(e.getMessage(), "/login-form");
+    }
+
+//    @ExceptionHandler(ExceptionApi401.class)
+//    public String exApi401(Exception e) {
+//        return Script.href(e.getMessage(), "login-form");
+//    }
+
+    //Forbidden - 권한 없음 (403)
+    @ExceptionHandler(Exception403.class)
+    public String ex403(Exception e) {
+        return Script.alert(e.getMessage());
+    }
+
+//    @ExceptionHandler(ExceptionApi403.class)
+//    public String exApi403(Exception e) {
+//        return Script.alert(e.getMessage());
+//    }
+
+    //Not Found - 자원 찾을 수 없음 (404)
+    @ExceptionHandler(Exception404.class)
+    public String ex404(Exception e) {
+        return Script.back(e.getMessage());
+    }
+
+//    @ExceptionHandler(ExceptionApi404.class)
+//    public String exApi404(Exception e) {
+//        return Script.back(e.getMessage());
+//    }
+
+    //Internal Server Error - 나머지 에러 (500)
+    @ExceptionHandler(Exception.class)
+    public String exUnKnow(Exception e) {
+        return Script.back(e.getMessage());
+    }
+}
---
+++ src/main/java/com/example/recruit_page_wwy/_core/error/ex/ExceptionApi401.java
@@ -0,0 +1,7 @@
+package com.example.recruit_page_wwy._core.error.ex;
+
+public class ExceptionApi401 extends RuntimeException {
+    public ExceptionApi401(String message) {
+        super(message);
+    }
+}
