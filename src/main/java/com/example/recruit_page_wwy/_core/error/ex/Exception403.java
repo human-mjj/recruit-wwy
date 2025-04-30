32728b628d2ad891cf20a4b734cfcd26b346f3ef
JM <lsaoe157@naver.com>
2025-04-30 12:20:32 +0900

    ex 폴더 생성

---
+++ src/main/java/com/example/recruit_page_wwy/_core/error/ex/Exception403.java
@@ -0,0 +1,7 @@
+package com.example.recruit_page_wwy._core.error.ex;
+
+public class Exception403 extends RuntimeException {
+    public Exception403(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/recruit_page_wwy/_core/intercetor/LoginIntercetor.java
+++ src/main/java/com/example/recruit_page_wwy/_core/intercetor/LoginIntercetor.java
@@ -1,14 +1,19 @@
 package com.example.recruit_page_wwy._core.intercetor;
 
+import com.example.recruit_page_wwy.user.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class LoginIntercetor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
+        String uri = req.getRequestURI();
+        System.out.println("uri: " + uri);
 
-
+        HttpSession session = req.getSession();
+        User sessionUser = (User) session.getAttribute("sessionUser");
         return true;
     }
 
