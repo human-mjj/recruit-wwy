8c582c77c64e182da1c04c19544c41ae08678acd
Seo Hoejeong <139827011+clubnerdy@users.noreply.github.com>
2025-05-15 16:40:59 +0900

    [feat] user DTO 전환 (#55)
    * change user DTO ing
    
    * ing
    
    * change DTO complete
    
    * img test complete
    
    ---------
    
    Co-authored-by: 사ìSeoHoejeong <parang@naver.com>

--- src/main/java/com/example/recruit_page_wwy/user/UserResponse.java
+++ src/main/java/com/example/recruit_page_wwy/user/UserResponse.java
@@ -5,6 +5,30 @@ import lombok.Data;
 public class UserResponse {
 
     @Data
+    public static class DTO {
+        private String username;
+        private String password;
+        private String email;
+        private boolean isCompanyUser;
+        private String phone;
+        private int role;
+        private String comName;
+        private Integer industryId;
+        private User user;
+
+        public DTO(User user) {
+            this.email = user.getEmail();
+            this.isCompanyUser = user.getRole() == 1;
+            this.phone = user.getPhone();
+            this.role = user.getRole();
+            this.comName = user.getComName();
+            this.industryId = user.getIndustry() != null ? user.getIndustry().getId() : null;
+            this.user = user;
+            this.username = user.getUsername();
+        }
+    }
+
+    @Data
     public static class MyPageDTO {
         private String username;
         private String email;
--- src/main/java/com/example/recruit_page_wwy/user/UserService.java
+++ src/main/java/com/example/recruit_page_wwy/user/UserService.java
@@ -19,28 +19,29 @@ public class UserService {
 
     // JWT는 Access Token만 사용 (만료기간 1시간)
 
-    // TODO : 저장 후 DTO에 담아서 반환
+    // TODO : 저장 후 DTO에 담아서 반환 ✅
     // TODO : BCrypt 사용해서 비밀번호 해시 생성
     // TODO : JWT 생성
     @Transactional
-    public void joinUser(UserRequest.UserDTO reqDTO) {
-        userRepository.saveUser(reqDTO.toEntity());
-
+    public UserResponse.DTO joinUser(UserRequest.UserDTO reqDTO) {
+        User userPS = userRepository.saveUser(reqDTO.toEntity());
+        return new UserResponse.DTO(userPS);
     }
 
-    // TODO : 저장 후 DTO에 담아서 반환
+    // TODO : 저장 후 DTO에 담아서 반환 ✅
     // TODO : BCrypt 사용해서 비밀번호 해시 생성
     // TODO : JWT 생성
     @Transactional
-    public void joinCom(UserRequest.ComDTO reqDTO) {
-        userRepository.saveCom(reqDTO.toEntity());
+    public UserResponse.DTO joinCom(UserRequest.ComDTO reqDTO) {
+        User userPS = userRepository.saveCom(reqDTO.toEntity());
+        return new UserResponse.DTO(userPS);
     }
 
-    // TODO : User가 아니라 DTO에 담아서 반환
+    // TODO : User가 아니라 DTO에 담아서 반환 ✅
     // TODO : BCrypt 사용해서 비밀번호 검증
     // TODO : JWT 검증
-    public User login(UserRequest.LoginDTO reqDTO) {
-        User user = userRepository.findByEmailAndPassword(reqDTO.getEmail(), reqDTO.getPassword());
+    public UserResponse.DTO login(UserRequest.LoginDTO reqDTO) {
+        User user = userRepository.findByEmail(reqDTO.getEmail());
 
         if (user == null) throw new ExceptionApi401("아이디 혹은 비밀번호가 틀렸습니다.");
 
@@ -51,15 +52,17 @@ public class UserService {
         if (user.getRole() != reqDTO.getRole()) {
             throw new ExceptionApi401("구분이 틀렸습니다.");
         }
-        return user;
+        return new UserResponse.DTO(user);
     }
 
-    // TODO : User가 아니라 DTO에 담아서 반환
+    // TODO : User가 아니라 DTO에 담아서 반환 ✅
+    // TODO : 이미지 null일 경우 안 올라가게 수정
+    // TODO : 이미지 Encoding 추가
     // 이 아래는 userUpdate 수업 때 진행하고 나서 하기
     // TODO : 비밀번호는 BCrypt 사용해서 해시 후 집어넣기
     // TODO : JWT 재발행 (파기 후 다시 발급)
     @Transactional
-    public UserResponse.UpdateDTO userUpdate(UserRequest.UpdateDTO reqDTO, User sessionUser) {
+    public UserResponse.DTO userUpdate(UserRequest.UpdateDTO reqDTO, User sessionUser) {
         User userPS = userRepository.findById(sessionUser.getId());
         String imgFilename = null;
 
@@ -81,11 +84,12 @@ public class UserService {
 
         // 더티 체킹
         userPS.update(reqDTO.toEntity(imgFilename));
-        return new UserResponse.UpdateDTO(userPS);
+        return new UserResponse.DTO(userPS);
     }
 
-    // TODO : User가 아니라 DTO에 담아서 반환
-    public User mypage(Integer id) {
-        return userRepository.findById(id);
+    // TODO : User가 아니라 DTO에 담아서 반환 ✅
+    public UserResponse.MyPageDTO mypage(Integer id) {
+        User user = userRepository.findById(id);
+        return new UserResponse.MyPageDTO(user);
     }
 }
