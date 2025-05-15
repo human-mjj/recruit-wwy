3e1b861bee1237e247ddb7dbd79ef17026ef75e3
사ìSeoHoejeong <parang@naver.com>
2025-05-15 17:56:13 +0900

    annotaion edit

--- src/main/java/com/example/recruit_page_wwy/user/UserController.java
+++ src/main/java/com/example/recruit_page_wwy/user/UserController.java
@@ -5,14 +5,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class UserController {
     private final UserService userService;
     private final HttpSession session;
