0205913f1d1523814e3149c78263bae551b10acd
JM <lsaoe157@naver.com>
2025-05-19 16:06:53 +0900

    apply save 제외 test 완료

--- src/main/java/com/example/recruit_page_wwy/apply/ApplyController.java
+++ src/main/java/com/example/recruit_page_wwy/apply/ApplyController.java
@@ -2,40 +2,33 @@ package com.example.recruit_page_wwy.apply;
 
 import com.example.recruit_page_wwy._core.util.Resp;
 import com.example.recruit_page_wwy.user.User;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class ApplyController {
     private final ApplyService applyService;
     private final HttpSession session;
 
     // TODO : 예외 처리
-    @GetMapping("/mypage/apply")
-    public String applyList(HttpServletRequest request) {
+    @GetMapping("/s/api/mypage/apply")
+    public ResponseEntity<?> applyList() {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        ApplyResponse.UserApplyListDTO userApplyList = applyService.findUserApply(sessionUser);
-        request.setAttribute("model", userApplyList);
-
-
-        return "resume/apply-list";
+        ApplyResponse.UserApplyListDTO respDTO = applyService.findUserApply(sessionUser);
+        return Resp.ok(respDTO);
     }
 
     // TODO : 예외 처리
-    @GetMapping("/mypage/apply/com")
-    public String applyManageList(HttpServletRequest request) {
+    @GetMapping("/s/api/mypage/com")
+    public ResponseEntity<?> applyManageList() {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        ApplyResponse.ComApplyListDTO comApplyList = applyService.findComApply(sessionUser);
-        request.setAttribute("model", comApplyList);
-
-        return "resume/com-apply-list";
+        ApplyResponse.ComApplyListDTO respDTO = applyService.findComApply(sessionUser);
+        return Resp.ok(respDTO);
     }
 
     // TODO : 예외 처리
@@ -49,12 +42,13 @@ public class ApplyController {
 
     // TODO : DTO 반환에서 Body에 넣기
     // TODO : Resp 수정
-    @PostMapping("/api/apply")
-    public @ResponseBody ResponseEntity<?> updateApplyProgress(@RequestBody Map<String, String> request) {
+    @PutMapping("/api/apply")
+    public ResponseEntity<?> updateApplyProgress(@RequestBody Map<String, String> request) {
         Integer applyId = Integer.valueOf(request.get("applyId"));
         String progress = request.get("progress");
 
         ApplyResponse.DTO respDTO = applyService.updateProgress(applyId, progress); // 실제 처리 로직
         return Resp.ok(respDTO);
     }
+
 }
